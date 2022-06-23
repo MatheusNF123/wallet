f7deaaa9a1914f142fa4e125dee5fa2d75aff650
Matheus <matheus_cs.nf@hotmail.com>
2022-06-23 01:21:16 -0300

    exercicio6

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,33 +4,13 @@ import propTypes from 'prop-types';
 import { actionCurrencieThunk } from '../actions';
 
 class Header extends React.Component {
-  // state = {
-  //   soma: 0,
-  // }
-
   componentDidMount() {
     const { curr } = this.props;
     curr();
-    this.somar();
-  }
-
-  somar = () => {
-    const { valores } = this.props;
-    const result = valores
-      ?.reduce((acc, elemento) => {
-        // const curr = elemento.exanchanges[elemento.currency].ask;
-        console.log(elemento.exanchanges);
-        // .find((element) => elemento.currency === element.code);
-        acc = Number(elemento.value) * Number(curr.ask);
-        return acc;
-      }, 0);
-    console.log(result);
-    // this.setState({ soma: result });
   }
 
   render() {
     const { email, soma = 0 } = this.props;
-    // const { soma } = this.state;
     return (
       <header>
         <div data-testid="email-field">{email}</div>
@@ -52,7 +32,6 @@ const mapDispatchToProps = (dispatch) => ({
 const mapStateToProps = (state) => ({
   email: state.user.email,
   moeda: state.wallet.currencies,
-  valores: state.wallet.expenses,
   soma: state.wallet.soma,
 });
 
